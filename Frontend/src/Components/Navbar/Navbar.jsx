8139173e00b4846bededdf1b068a12a26f48d011
Srujan Bali <106592396+Moonwalker1005@users.noreply.github.com>
2024-11-26 13:06:05 +0530

    Search Functionality Added on the Frontend side

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -46,12 +46,14 @@ function NavBar() {
       </Link>
       <ul className = {Sdisplay} ref={Navref}>
         <li onClick={hidesidebar} className="close-icon" ><img src={closeicon} alt="Close" /></li>
+        <ActivePage to="/search"> Search Books</ActivePage>
         <ActivePage to="/">Books Catalogue</ActivePage>
         <ActivePage to="/contact">Contact</ActivePage>
         <ActivePage to="/about">About Us</ActivePage>
         <ActivePage to="/addbook">Add A Book</ActivePage>
       </ul>
-      <ul>
+      <ul className="sidebar">
+        <input type="text" placeholder="Search Books" className="hideOnMobile"/>
         <ActivePage className = "hideOnMobile" to="/">Books Catalogue</ActivePage>
         <ActivePage className = "hideOnMobile" to="/contact">Contact</ActivePage>
         <ActivePage className = "hideOnMobile" to="/about">About Us</ActivePage>
---
+++ Frontend/src/Components/Pages/Search.jsx
@@ -0,0 +1,9 @@
+import "./Page_CSS/Search.css"
+
+export default function Search(){
+    return(
+        <div className="search">
+            <input type="text" placeholder = "Search Books" />
+        </div>
+    )
+}
